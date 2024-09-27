cb8abf7a605dd84d18e069ebc8feb801ee14fe07
yoonhayu <yoonfoy54@gmail.com>
2024-09-27 09:42:13 +0900

    두번째 연습

---
+++ Desktop/SBS IT Ac/pratice_2/src/main/java/org/example/article/Article.java
@@ -0,0 +1,30 @@
+package org.example.article;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.user.SiteUser;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 200)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+    private LocalDateTime modifyDate;
+
+    @ManyToOne
+    private SiteUser author;
+
+}
---
+++ Desktop/SBS IT Ac/pratice_2/src/main/java/org/example/article/ArticleService.java
@@ -0,0 +1,54 @@
+package org.example.article;
+
+import lombok.RequiredArgsConstructor;
+import org.example.user.SiteUser;
+import org.example.user.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+    private final ArticleRepository articleRepository;
+    private final UserRepository userRepository;
+
+    public void create(String title, String content, Principal principal) {
+        Article article = new Article();
+        article.setTitle(title);
+        article.setContent(content);
+        article.setCreateDate(LocalDateTime.now());
+        Optional<SiteUser> siteUser = userRepository.findByUsername(principal.getName());
+        if (siteUser.isPresent()) {
+            article.setAuthor(siteUser.get());
+        } else {
+            throw new NoSuchElementException("Author not found");
+        }
+
+
+        this.articleRepository.save(article);
+
+    }
+
+    public Article getArticle(Integer id) {
+        Optional<Article> article = this.articleRepository.findById(id);
+        return article.get();
+    }
+
+    public List<Article> getList() {
+        return this.articleRepository.findAll();
+    }
+    public void modify(Article article, String title, String cotent){
+        article.setTitle(title);
+        article.setContent(cotent);
+        article.setModifyDate(LocalDateTime.now());
+        this.articleRepository.save(article);
+    }
+    public void delete(Article article){
+        this.articleRepository.delete(article);
+    }
+}
